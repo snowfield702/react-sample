8e11dc78e7f1a7a9a4beee1ee0598e829ed66cd7
snowfield702 <snowfield702@gmail.com>
2019-03-18 14:37:15 +0900

    App.cssをstyled-componentsで書き直す

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import styled, { keyframes } from 'styled-components';
+
+const AppRoot = styled.div`
+  text-align: center;
+`;
+
+const AppHeader = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+const AppLogoSpin = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const AppLogo = styled.img`
+  animation: ${AppLogoSpin} infinite 20s linear;
+  height: 40vmin;
+  pointer-events: none;
+`;
+
+const AppLink = styled.a`
+  color: #61dafb;
+`;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <AppRoot>
+        <AppHeader>
+          <AppLogo src={logo} alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <AppLink href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
             Learn React
-          </a>
-        </header>
-      </div>
+          </AppLink>
+        </AppHeader>
+      </AppRoot>
     );
   }
 }
